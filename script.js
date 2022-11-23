ce7c21bcc7d1270191d1ec9453642bddea36b48c
Yash1Hingu <108277321+Yash1Hingu@users.noreply.github.com>
2022-11-23 21:41:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function calcultion(){
+    var a,b,c,choice;
+    choice = document.getElementById("opertion").value;
+    a = Number(document.getElementById("firstnumber").value);
+    b = Number(document.getElementById("secoundNumber").value);
+    
+    if(choice == "Addition")
+    {
+        c = a+b;
+        console.log(c);
+        document.getElementById("answer").value = c;
+    }
+    else if(choice == "Subtraction")
+    {
+        c = a-b;
+        document.getElementById("answer").value = c;
+    }
+    else if(choice == "division")
+    {
+        c = a/b;
+        document.getElementById("answer").value = c;
+    }
+    else if(choice == "multiplaction"){
+        c = a*b;
+        document.getElementById("answer").value = c;
+    }
+    else{
+        document.getElementById("answer").value = "Invalid Choice"
+    }
+}
